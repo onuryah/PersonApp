02e826014fff15e86e05eba7c874fc0218f46e16
Onuralp <oyahnici@mobiversite.com>
2023-04-07 23:57:40 +0300

    Inject ColletionView

---
+++ PersonApp/Scene/MainVC.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  PersonApp
+//
+//  Created by Macbook on 7.04.2023.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension MainVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
---
+++ PersonApp/Scene/PersonCell/PersonCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonCell.swift
+//  PersonApp
+//
+//  Created by Macbook on 7.04.2023.
+//
+
+import UIKit
+
+class PersonCell: UITableViewCell {
+    @IBOutlet weak var personLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
