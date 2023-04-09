734bebcd01ac2005be71516a940586a31870a014
Onuralp <oyahnici@mobiversite.com>
2023-04-09 17:23:11 +0300

    Add retry when get fail

--- PersonApp/Scene/MainVM.swift
+++ PersonApp/Scene/MainVM.swift
@@ -19,7 +19,10 @@ class MainVM: MainBussinessLayer {
     var personArray: [Person] = []
     let group = DispatchGroup()
     var delegate: BaseDelegateProtocol?
-    var pagination: String? = nil
+    var pagination: String? = "0"
+    var delay = 3.0
+    var delayIncreaser = 3.0
+    
     func fetchData() {
         group.enter()
         DataSource.fetch(next: pagination) {[weak self] response, error in
@@ -27,6 +30,7 @@ class MainVM: MainBussinessLayer {
             if error != nil {
                 if let errorMessage = error?.errorDescription {
                     self.delegate?.createFailureAlert(failMessage: errorMessage)
+                    self.retry()
                 }
             } else {
                 if let next = response?.next {
@@ -35,7 +39,19 @@ class MainVM: MainBussinessLayer {
                 response?.people.forEach({ person in
                     self.addUniquePerson(person: person)
                 })
+                self.delay = 3.0
             }
+            self.group.leave()
+        }
+    }
+    
+    func retry() {
+        group.enter()
+        DispatchQueue.global().asyncAfter(deadline: .now() + delay) {[weak self] in
+            guard let self = self else { return }
+            print(self.delay)
+            self.delay += self.delayIncreaser
+            self.fetchData()
             self.group.leave()
         }
     }
